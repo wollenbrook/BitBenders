ed95a820e5060ad844f18de45c8e20ca07faeea4
Miguel Medina <mmedinaortiz20@mail.wou.edu>
2024-02-18 17:27:50 -0800

    commiting a semi complete version of this feature

--- main/BitBracket/src/BitBracket/Controllers/HomeController.cs
+++ main/BitBracket/src/BitBracket/Controllers/HomeController.cs
@@ -1,24 +1,46 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using BitBracket.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 
 namespace BitBracket.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly UserManager<IdentityUser> _userManager;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, UserManager<IdentityUser> userManager)
     {
         _logger = logger;
+        _userManager = userManager;
     }
 
     public IActionResult Index()
     {
+
         return View();
     }
     public IActionResult Announcement()
     {
+        return View();
+    }
+    
+    public async Task<IActionResult> Profile()
+    { 
+        // info from controller: non database info
+        bool isAuthenticated = User.Identity.IsAuthenticated;
+        string name = User.Identity.Name;
+        string AuthType = User.Identity.AuthenticationType;
+
+        // info from identity
+        string id = _userManager.GetUserId(User);
+
+        IdentityUser user = await _userManager.GetUserAsync(User);
+        string email = user?.Email ?? "no email";
+        string phone = user?.PhoneNumber ?? "no phone number";
+        ViewBag.Message = $"user {name} is authenticated: {isAuthenticated} using {AuthType}, ID is {id} email is: {email}";
         return View();
     }
 
---
+++ main/BitBracket/src/BitBracket/Scripts/test.sql
@@ -0,0 +1,2 @@
+Select TOP (10) [Id], [UserName]
+From [aspnet-BitBracket-6a207161-df86-4385-9c80-f8f7b4d056e9].[dbo].AspNetUsers
