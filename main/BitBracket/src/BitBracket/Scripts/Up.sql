73b2368231f7d5073a95e0319a3a28ae8e7b7a03
wollenbrook <wollenbrook21@mail.wou.edu>
2024-03-21 21:04:00 -0700

    Fixing duplicate BitUser table in UP script

--- main/BitBracket/src/BitBracket/Scripts/Up.sql
+++ main/BitBracket/src/BitBracket/Scripts/Up.sql
@@ -1,12 +1,27 @@
+CREATE TABLE [BitUser] (
+	[ID] INT PRIMARY KEY IDENTITY(1,1),
+    [ASPNetIdentityID] NVARCHAR(50) NOT NULL,
+	[Username] NVARCHAR(50) NOT NULL,
+    [Tag] NVARCHAR(50) NOT NULL,
+    [Bio] NVARCHAR(500) NOT NULL,
+    [ProfilePicture] VARBINARY(MAX) NULL,
+    [EmailConfirmedStatus] BIT NULL
+);
 CREATE TABLE [Tournaments] (
     [ID] int PRIMARY KEY IDENTITY(1, 1),
     [Name] nvarchar(50) NOT NULL,
     [Location] nvarchar(255) NOT NULL,
     [Status] nvarchar(50) NOT NULL,
-    [Created] datetime NOT NULL
+    [Created] datetime NOT NULL,
     [Owner] int FOREIGN KEY REFERENCES [BitUser]([ID])
 );
-
+CREATE TABLE [Brackets] (
+    [ID] INT PRIMARY KEY IDENTITY(1,1),
+    [Name] NVARCHAR(50) NOT NULL,
+    [Status] NVARCHAR(50) NOT NULL,
+    [BracketData] NVARCHAR(4000) NOT NULL,
+    [TournamentID] INT FOREIGN KEY REFERENCES [Tournaments]([ID])
+);
 CREATE TABLE [Announcements] (
     [ID] INT IDENTITY(1,1) PRIMARY KEY,
     [Title] NVARCHAR(50) NOT NULL, 
@@ -14,21 +29,4 @@ CREATE TABLE [Announcements] (
     [Description] NVARCHAR(500) NOT NULL, 
     [IsActive] BIT NOT NULL,
     [Author] NVARCHAR(50) NOT NULL 
-);
-
-CREATE TABLE [BitUser] (
-	[ID] INT PRIMARY KEY IDENTITY(1,1),
-    [ASPNetIdentityID] NVARCHAR(50) NOT NULL,
-	[Username] NVARCHAR(50) NOT NULL,
-    [Tag] NVARCHAR(50) NOT NULL,
-    [Bio] NVARCHAR(500) NOT NULL,
-    [ProfilePicture] VARBINARY(MAX) NULL
-);
-
-CREATE TABLE [Brackets] (
-    [ID] INT PRIMARY KEY IDENTITY(1,1),
-    [Name] NVARCHAR(50) NOT NULL,
-    [Status] NVARCHAR(50) NOT NULL,
-    [BracketData] NVARCHAR(4000) NOT NULL,
-    [TournamentID] INT FOREIGN KEY REFERENCES [Tournaments]([ID])
 );
