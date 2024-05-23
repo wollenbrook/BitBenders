2fe746c58dfae44bc6b8375dc68226411eb39ee6
Miguel Medina <mmedinaortiz20@mail.wou.edu>
2024-05-22 19:41:08 -0700

    commiting a working broadcast display on the user facing tournament pages

--- main/BitBracket/src/BitBracket/wwwroot/js/tournamentProfile.js
+++ main/BitBracket/src/BitBracket/wwwroot/js/tournamentProfile.js
@@ -1,3 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const userId = document.getElementById('userId').value;
+    const tournamentId = document.getElementById('tournamentId').value;
+
+    checkParticipationStatus(tournamentId, userId);
+}
+);
+
+
+
+
 function sendParticipationRequest(tournamentId, userId) {
     fetch(`/api/TournamentAPI/SendRequest/${userId}/${tournamentId}`, {
         method: 'POST'
